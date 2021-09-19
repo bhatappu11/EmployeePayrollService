801153ae861ec86afd1d791a24875a72a49fe0bd
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-19 22:20:10 +0530

    ADDED: test case to initiate watch service

--- EmployeePayrollService/src/test/java/com/bridgelabz/employeepayrollservice/NIOFileAPITest.java
+++ EmployeePayrollService/src/test/java/com/bridgelabz/employeepayrollservice/NIOFileAPITest.java
@@ -47,4 +47,10 @@ public class NIOFileAPITest {
 		}
 		
     }
+    @Test
+    public void givenAsDirectoryWhenWatchedListsAllTheActivities() throws IOException {
+    	Path dir = Paths.get(HOME+"/"+PLAY_WITH_NIO);
+    	Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+    	new Java8WatchServiceExample(dir).processEvents();
+    }
 }
