07fff5ab94f6901e7f5521cf30fd2dc493fd9b92
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-19 21:55:05 +0530

    ADDED: test to delete file and check file not exist

--- EmployeePayrollService/src/test/java/com/bridgelabz/employeepayrollservice/NIOFileAPITest.java
+++ EmployeePayrollService/src/test/java/com/bridgelabz/employeepayrollservice/NIOFileAPITest.java
@@ -15,6 +15,11 @@ public class NIOFileAPITest {
     public void givenPath_performFileOperations_Confirm() {
     	Path homePath=Paths.get(HOME);
         Assert.assertTrue(Files.exists(homePath));
+        
+        Path playPath=Paths.get(HOME+"/"+PLAY_WITH_NIO);
+        if(Files.exists(playPath))
+        	FileUtils.deleteFiles(playPath.toFile());
+        Assert.assertTrue(Files.notExists(playPath));
 
     }
 }
